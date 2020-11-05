309f0b02a4fa95f9dbb1842b51690facf0e40807
lrlrlrlr <acecardii@qq.com>
2020-11-05 12:18:19 +1100

    add more clfs to compare list

--- main.py
+++ main.py
@@ -13,36 +13,53 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 import matplotlib.pyplot as plt
 # load the images
 from ultilities import imgs,img_labels
 X_train,X_test,y_train,y_test = train_test_split(imgs,img_labels, test_size=0.25, shuffle=True)
 
-# preprocessing
-# get the features(in same dimension) imagearray + metadata
+    # preprocessing
+    # get the features(in same dimension) imagearray + metadata
 
 
 
-# train models
-# compare different clf
-classifiers = [KNeighborsClassifier(),
-               DecisionTreeClassifier(),
-               LinearDiscriminantAnalysis(),
-               LogisticRegression(),
-               GaussianNB(),
-               SVC()]
+    # train models
+    # compare different clf
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()]
 
-classifier_accuracy_list = []
-for i, classifier in enumerate(classifiers):
-    # split the dataset into 5 folds; then test the classifier against each fold one by one
-    accuracies = cross_val_score(classifier, imgs, img_labels.ravel(), cv=5)
-    classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
+    classifier_accuracy_list = []
+    for i, classifier in enumerate(classifiers):
+        # split the dataset into 5 folds; then test the classifier against each fold one by one
+        accuracies = cross_val_score(classifier, imgs, img_labels.ravel(), cv=5)
+        classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
 
-# sort the classifiers
-classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
-for item in classifier_accuracy_list:
-    print(item[1], ':', item[0])
+    # sort the classifiers
+    classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
+    for item in classifier_accuracy_list:
+        print(item[1], ':', item[0])
 
 
 
