994204d5a805dd965b2c17c26ed79be5a5ca601c
lrlrlrlr <acecardii@qq.com>
2020-11-05 17:09:27 +1100

    surf feature extraction

---
+++ attempt_resize.py
@@ -0,0 +1,69 @@
+# import
+import cv2 as cv
+import os
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, accuracy_score, recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+import matplotlib.pyplot as plt
+# load the images
+from ultilities import *
+for size in [50,100,200,300]:
+
+    # preprocessing
+    print('Resize the image to: {}*{}'.format(size,size))
+    imgs,img_labels = resize_img((size,size))
+    X_train,X_test,y_train,y_test = train_test_split(imgs,img_labels, test_size=0.25, shuffle=True)
+
+
+
+
+    # train models
+    # compare different clf
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()]
+
+    classifier_accuracy_list = []
+    for i, classifier in enumerate(classifiers):
+        # split the dataset into 5 folds; then test the classifier against each fold one by one
+        accuracies = cross_val_score(classifier, imgs, img_labels.ravel(), cv=5)
+        classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
+
+    # sort the classifiers
+    classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
+    for item in classifier_accuracy_list:
+        print(item[1], ':', item[0])
+
+
+
+
--- ultilities.py
+++ ultilities.py
@@ -13,19 +13,20 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-
+from skimage.feature import hog
 import matplotlib.pyplot as plt
 
 # load the images
 path_arab = "D:\OneDrive - UNSW\COMP9517\project\Plant\Ara2013-Canon"
 path_toba = "D:\OneDrive - UNSW\COMP9517\project\Plant\Tobacco"
+path_toba_segmented = "segmented"
+toba_filenames_segmented = [pic for pic in os.listdir(path_toba_segmented)]
 toba_filenames = [pic for pic in os.listdir("D:\OneDrive - UNSW\COMP9517\project\Plant\Tobacco") if '_rgb.png' in pic]
 arab_filenames = [pic for pic in os.listdir("D:\OneDrive - UNSW\COMP9517\project\Plant\Ara2013-Canon") if
                   '_rgb.png' in pic]
 
 
 def resize_img(imgsize=(50 * 50)):
-    # print the shape
     arab_imgs = []
     toba_imgs = []
 
@@ -34,6 +35,7 @@ def resize_img(imgsize=(50 * 50)):
         img = cv.resize(img, imgsize)
         img = img.flatten()
         arab_imgs.append(img)
+
     for filename in toba_filenames:
         img = cv.imread(path_toba + '\\' + filename, 0)
 
@@ -44,7 +46,52 @@ def resize_img(imgsize=(50 * 50)):
     imgs = np.array(arab_imgs + toba_imgs)
     img_labels = np.array(len(arab_imgs) * [True] + len(toba_imgs) * [False]).reshape(-1,
                                                                                       1)  # True means arabidopsis, False means tobacco
-    return imgs, img_labels
+    return imgs, img_labels # return image as feature
+
+def hog_img(segmented=True):
+    if segmented:
+        filenames = toba_filenames_segmented
+        path = path_toba_segmented
+    else:
+        filenames = toba_filenames
+        path = path_toba
+    # load the img
+    features = []
+    labels_is_tobbaco = []
+    for filename in filenames:
+        img = cv.imread(path + '\\' + filename)
+        fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
+                            cells_per_block=(1, 1), visualize=True, multichannel=True)
+        features.append(hog_image.flatten())
+        labels_is_tobbaco.append(True)
+
+    for filename in arab_filenames:
+        img = cv.imread(path_arab + '\\' + filename)
+        fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
+                            cells_per_block=(1, 1), visualize=True, multichannel=True)
+        features.append(hog_image.flatten())
+        labels_is_tobbaco.append(False)
+    return np.array(features),np.array(labels_is_tobbaco)
+
+def surf_features(hessianThreshold=1000):
+    features = []
+    labels_is_tobbaco = []
+
+    surf = cv.xfeatures2d.SURF_create(hessianThreshold)
+
+    for filename in toba_filenames:
+        img = cv.imread(path_toba + '\\' + filename, 0)
+        kp, des = surf.detectAndCompute(img, None)
+        features.append(des[:5].flatten())
+        labels_is_tobbaco.append(True)
+
+    for filename in arab_filenames:
+        img = cv.imread(path_arab + '\\' + filename, 0)
+        kp, des = surf.detectAndCompute(img, None)
+        features.append(des[:5].flatten())
+        labels_is_tobbaco.append(False)
+
+    return np.array(features),np.array(labels_is_tobbaco)
 
 def split_data(X,y,split_percentage=0.75):
     split_point = int(len(X) * split_percentage)
@@ -53,3 +100,10 @@ def split_data(X,y,split_percentage=0.75):
     X_test = X[split_point:]
     y_test = y[split_point:]
     return X_train, y_train, X_test, y_test
+
+if __name__ == '__main__':
+    # X, y = hog_img()
+    img = cv.imread(path_toba + '\\' + toba_filenames[0], 0)
+    img_resized = cv.resize(img, (300 , 300))
+
+    X,y = surf_features()
