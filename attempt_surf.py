591b776dc302db6e0d9d53f619a232170d1ac3c2
lrlrlrlr <acecardii@qq.com>
2020-11-06 12:48:18 +1100

    fix: surf can process segmented pics

--- attempt_surf.py
+++ attempt_surf.py
@@ -25,46 +25,58 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import warnings
 
 import matplotlib.pyplot as plt
 # load the images
 from ultilities import *
 
-imgs,img_labels = surf_features()
-X_train,X_test,y_train,y_test = train_test_split(imgs,img_labels, test_size=0.25, shuffle=True)
 
-# preprocessing
-# get the features(in same dimension) imagearray + metadata
-
-
-
-# train models
-# compare different clf
-classifiers = [
-    KNeighborsClassifier(),
-    SVC(),
-    GaussianProcessClassifier(),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    MLPClassifier(),
-    AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()
+def compare_diff_models(X, y):
+    # # train models
+    # # compare different clf
+    classifiers = [
+        KNeighborsClassifier(),
+        SVC(),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        MLPClassifier(),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()
     ]
 
-classifier_accuracy_list = []
-for i, classifier in enumerate(classifiers):
-    # split the dataset into 5 folds; then test the classifier against each fold one by one
-    accuracies = cross_val_score(classifier, imgs, img_labels.ravel(), cv=5)
-    classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
+    classifier_accuracy_list = []
+    for i, classifier in enumerate(classifiers):
+        # split the dataset into 5 folds; then test the classifier against each fold one by one
+        accuracies = cross_val_score(classifier, X, y.ravel(), cv=5)
+        classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
 
-# sort the classifiers
-classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
-for item in classifier_accuracy_list:
-    print(item[1], ':', item[0])
-
-
-
+    # sort the classifiers
+    # classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
+    classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
+    for item in classifier_accuracy_list:
+        print(item[1], ':', item[0])
 
 
+if __name__ == '__main__':
+    # for n in [1000,500,1500]:
+    #     print("hessianThreshold: ", n)
+    imgs, img_labels = surf_features(segmented=True)
+    X_train, X_test, y_train, y_test = train_test_split(np.array(imgs), np.array(img_labels), test_size=0.25,
+                                                        shuffle=True)
 
+    clf = SVC()
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    print("accuracy:\t", accuracy_score(y_test, predictions))
+    print("recall:\t\t", recall_score(y_test, predictions, average='macro'))
+
+    from sklearn.metrics import roc_auc_score
+    y_scores = clf.decision_function(X_test)
+    print("AUC:", roc_auc_score(y_test, y_scores))
+
+    from sklearn.metrics import f1_score
+
+    print("f1:", f1_score(y_test, predictions, zero_division=1))
